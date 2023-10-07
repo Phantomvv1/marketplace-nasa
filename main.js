2e63700d6e1ce48aa94b7f713bb9783bc8ddc96a
phantomvv1 <krisi_andreev@abv.bg>
2023-10-07 18:40:25 +0300

    Changes

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-document.getElementById("Search").addEventListener("mousedown", () => {
-    let navbar = document.getElementById("navbar");
-    document.getElementById("Search").style.zIndex = navbar;
+let navbar = document.getElementById("navbar");
+
+let search = document.getElementById("searchBlock");
+
+let searchElement = document.getElementById("Search");
+
+searchElement.addEventListener("mousedown", () => {
+    let x = document.createElement("input");
+    x.setAttribute("type", "text");
+    navbar.appendChild(x);
+    navbar.replaceChild(x, searchBlock);
+    x.focus();
+    x.addEventListener("blur", () => {
+        // When the input box loses focus, replace it with the "Search" text
+        navbar.appendChild(search);
+        navbar.replaceChild(searchBlock, x);
+    });
 });
